7478db335a487980eec0439ba3317d60207dee10
Andriy <andygrigs88@gmail.com>
2024-01-23 09:33:55 +0200

    ++

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -2,15 +2,22 @@
 import './App.css'
 
 import { FC, useState } from 'react';
+// import { increment } from './store/reducers/userSlice';
+import { useAppDispatch, useAppSelector } from './hooks/reduxHooks';
+
 
 
 
 
 export const App: FC = () => {
-
+  // const { count } = useAppSelector(state => state.userReducer)
+  const dispatch = useAppDispatch()
+  // const handleIncrement = () => {
+  //   dispatch(increment(1));
+  // };
   return (
     <div className="App">
-      Hello
+      {/* <button onClick={handleIncrement}>{count}</button> */}
     </div>
   );
 };
--- redux/src/store/reducers/userSlice.ts
+++ redux/src/store/reducers/userSlice.ts
@@ -1,20 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser"
 
 interface userState {
     users: IUser[];
     isLoading: boolean;
     error: string;
+    // count: number;
 }
 
 const initialState: userState = {
     users: [],
     isLoading: false,
-    error: ''
+    error: '',
+    // count: 0
 }
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {}
+    reducers: {
+        // increment(state, action: PayloadAction<number>) {
+        //     state.count += action.payload
+        // }
+    }
 })
+// export const { increment } = userSlice.actions;
+export default userSlice.reducer;
